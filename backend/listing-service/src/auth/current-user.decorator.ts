e08a07c7a8d511a87179441df5fcd321700e0692
jarylozh <jarylozh@gmail.com>
2026-03-20 09:46:47 +0800

    fix: use keyof CognitoJwtPayload to resolve unnecessary type assertion lint error

--- backend/listing-service/src/auth/current-user.decorator.ts
+++ backend/listing-service/src/auth/current-user.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { CognitoJwtPayload } from "./types/cognito-jwt-payload";
 
 export const CurrentUser = createParamDecorator(
-  (data: string | undefined, ctx: ExecutionContext) => {
+  (data: keyof CognitoJwtPayload | undefined, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest<Express.Request>();
     const user = request.user;
 
