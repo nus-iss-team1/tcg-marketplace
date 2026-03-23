0c22b36ac91cda5a3c500b0a151b7e0306779c3a
JingKai <jingkaihere@hotmail.com>
2026-03-23 23:04:29 +0800

    include service in health check

--- backend/listing-service/src/app.controller.ts
+++ backend/listing-service/src/app.controller.ts
@@ -8,6 +8,9 @@ export class AppController {
   @Public()
   @Get("health")
   healthCheck() {
-    return { status: "ok" };
+    return {
+      service: "listing-service",
+      status: "ok"
+    };
   }
 }
