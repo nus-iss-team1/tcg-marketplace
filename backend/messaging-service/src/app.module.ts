7a5b95d59ae7608553f1053e30c0a03f12c42c58
Jaryl <49894234+jarylozh@users.noreply.github.com>
2026-04-02 09:24:04 +0800

    chore: reverted required env schema

--- backend/messaging-service/src/app.module.ts
+++ backend/messaging-service/src/app.module.ts
@@ -17,9 +17,12 @@ import { MessagingGateway } from "./web-socket/messaging.gateway";
       isGlobal: true,
       envFilePath: ".env",
       validationSchema: Joi.object({
+        TZ: Joi.string().required(),
         LOG_DIR: Joi.string().required(),
         LOG_LEVEL: Joi.string().required(),
         AWS_REGION: Joi.string().required(),
+        AWS_ACCESS_KEY_ID: Joi.string().required(),
+        AWS_SECRET_ACCESS_KEY: Joi.string().required(),
         COGNITO_USER_POOL_ID: Joi.string().required(),
         COGNITO_APP_CLIENT_ID: Joi.string().required(),
         MESSAGING_PLATFORM_TABLE: Joi.string().default("MessagingPlatform"),
